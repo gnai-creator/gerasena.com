5c74ca6f79bab82587e00e24a1de3f58e78527c3
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-08-03 04:20:03 -0300

    Remove o logo da Mega-Sena e adiciona a imagem do novo logo na página inicial.

--- gerasena.com/src/app/page.tsx
+++ gerasena.com/src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-6">
+      <Image src="/logo.png" alt="Gerasena" width={100} height={100} />
       <h1 className="text-2xl font-bold">Gere Jogos da Mega-Sena</h1>
       <div className="flex gap-4">
         <Link
