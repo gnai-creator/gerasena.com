4091cfa72a209395b193cfebd77a9cd4341fb400
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-08-04 05:23:25 -0300

    Reset QTD_GERAR and QTD_GERAR_MAX to default values of 100 and 1000 respectively

--- gerasena.com/src/lib/constants.ts
+++ gerasena.com/src/lib/constants.ts
@@ -2,10 +2,10 @@ export const QTD_HIST = 100;
 
 // Quantidade de jogos a serem gerados pelo algoritmo genético.
 // Pode ser ajustado via variável de ambiente `NEXT_PUBLIC_QTD_GERAR`.
-export const QTD_GERAR = 10000;
+export const QTD_GERAR = 100;
 
 // Limite máximo de jogos que podem ser gerados pelo usuário.
-export const QTD_GERAR_MAX = 100000;
+export const QTD_GERAR_MAX = 1000;
 
 
 export const SITE_URL = "https://gerasena.com";
