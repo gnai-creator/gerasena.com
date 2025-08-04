a6cf668c471be7062145f3a65eb2f88719b286c8
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-08-04 17:52:22 -0300

    Enhance input styling for better visibility and user experience

--- gerasena.com/src/app/page.tsx
+++ gerasena.com/src/app/page.tsx
@@ -22,69 +22,68 @@ export default function Home() {
         <h1 className="text-2xl font-bold">Gerasena</h1>
         <h1 className="text-2xl font-bold">Gere Jogos da Mega-Sena</h1>
         <div className="flex gap-4 flex-col">
-        <input
-          type="number"
-          value={concurso}
-          onChange={(e) => setConcurso(e.target.value)}
-          placeholder="Concurso base"
-          className="rounded border px-4 py-2"
-        />
-        <input
-          type="number"
-          value={qtd}
-          onChange={(e) => {
-            const val = e.target.value;
-            if (!val) {
-              setQtd("");
-            } else {
-              const num = Math.min(parseInt(val, 10), QTD_GERAR_MAX);
-              setQtd(num.toString());
-            }
-          }}
-          placeholder="Quantidade de jogos"
-          className="rounded border px-4 py-2"
-          min={1}
-          max={QTD_GERAR_MAX}
-        />
-        <Link
-          href={`/manual${query ? `?${query}` : ""}`}
-          className="rounded bg-blue-600 px-4 py-2 text-white text-center hover:bg-blue-700"
-        >
-          Manual
-        </Link>
-        <Link
-          href={`/automatico${query ? `?${query}` : ""}`}
-          className="rounded bg-green-600 px-4 py-2 text-white text-center hover:bg-green-700"
-        >
-          Automático
-        </Link>
-        <Link
-          href="/historico"
-          className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
-        >
-          Histórico
-        </Link>
-        <Link
-          href="/estatisticas"
-          className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
-        >
-          Estatísticas
-        </Link>
-        <Link
-          href="/arquivo"
-          className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
-        >
-          Calcular Arquivo
-        </Link>
-        <a
-          href="https://github.com/gnai-creator/gerasena.com"
-          className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
-        >
-          Código Fonte
-        </a>
+          <input
+            type="number"
+            value={concurso}
+            onChange={(e) => setConcurso(e.target.value)}
+            placeholder="Concurso base"
+            className="rounded border px-4 py-2 bg-black text-white"
+          />
+          <input
+            type="number"
+            value={qtd}
+            onChange={(e) => {
+              const val = e.target.value;
+              if (!val) {
+                setQtd("");
+              } else {
+                const num = Math.min(parseInt(val, 10), QTD_GERAR_MAX);
+                setQtd(num.toString());
+              }
+            }}
+            placeholder="Quantidade de jogos"
+            className="rounded border px-4 py-2 bg-black text-white"
+            min={1}
+            max={QTD_GERAR_MAX}
+          />
+          <Link
+            href={`/manual${query ? `?${query}` : ""}`}
+            className="rounded bg-blue-600 px-4 py-2 text-white text-center hover:bg-blue-700"
+          >
+            Manual
+          </Link>
+          <Link
+            href={`/automatico${query ? `?${query}` : ""}`}
+            className="rounded bg-green-600 px-4 py-2 text-white text-center hover:bg-green-700"
+          >
+            Automático
+          </Link>
+          <Link
+            href="/historico"
+            className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
+          >
+            Histórico
+          </Link>
+          <Link
+            href="/estatisticas"
+            className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
+          >
+            Estatísticas
+          </Link>
+          <Link
+            href="/arquivo"
+            className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
+          >
+            Calcular Arquivo
+          </Link>
+          <a
+            href="https://github.com/gnai-creator/gerasena.com"
+            className="rounded bg-gray-600 px-4 py-2 text-white text-center hover:bg-gray-700"
+          >
+            Código Fonte
+          </a>
         </div>
       </main>
     </>
   );
 }
-
