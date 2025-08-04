bf038e7b4f5297c50cb1059a68cf152d5985702a
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-08-04 05:17:33 -0300

    Increase scraper interval from 15 seconds to 2 minutes

--- gerasena.com/src/lib/constants.ts
+++ gerasena.com/src/lib/constants.ts
@@ -16,4 +16,4 @@ export const CAIXA_API_BASE =
 export const MEGA_SENA_PAGE =
   "https://loterias.caixa.gov.br/Paginas/Mega-Sena.aspx";
 
-export const SCRAPER_INTERVAL_MS = 15000;
+export const SCRAPER_INTERVAL_MS = 120000;
