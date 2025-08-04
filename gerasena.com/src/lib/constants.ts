82d15a69ee552e34d6bc004122eb03322fe15b9b
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-08-04 05:04:03 -0300

    fix: revert QTD_GERAR to a static value of 10000

--- gerasena.com/src/lib/constants.ts
+++ gerasena.com/src/lib/constants.ts
@@ -2,7 +2,4 @@ export const QTD_HIST = 100;
 
 // Quantidade de jogos a serem gerados pelo algoritmo genético.
 // Pode ser ajustado via variável de ambiente `NEXT_PUBLIC_QTD_GERAR`.
-export const QTD_GERAR = parseInt(
-  process.env.NEXT_PUBLIC_QTD_GERAR || "100",
-  10,
-);
+export const QTD_GERAR = 10000;
